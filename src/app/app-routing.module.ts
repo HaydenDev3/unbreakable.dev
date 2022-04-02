4bbcf952e5de6842f73b2f728b4d95b43f4a7c70
CodeSocket <66907684+lyonation@users.noreply.github.com>
2022-04-02 13:50:59 +1000

    Projects Page finished, for now.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ProjectsComponent } from "./projects/projects.component";
+import { HomeComponent } from "./home/home.component";
+
 const routes: Routes = [
-  // { path: "/discord_server", component: "" }
+  { path: "", component: HomeComponent },
+  // Project Page.
+  { path: "projects", component: ProjectsComponent } // <domain>/projects
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, // Importing from App-routing.module.ts
   ],
   providers: [],
   bootstrap: [AppComponent]
