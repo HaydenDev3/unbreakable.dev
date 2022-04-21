d98e912b0ac86f4bb3c3b071a04887d994247ca0
CodeSocket <66907684+lyonation@users.noreply.github.com>
2022-04-21 14:47:38 +1000

    ✅ | Added animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,45 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+          'fade-in-down': {
+              '0%': {
+                  opacity: '0',
+                  transform: 'translateY(-10px)'
+              },
+              '100%': {
+                  opacity: '1',
+                  transform: 'translateY(0)'
+              },
+          },
+          'fade-in-up': {
+            '0%': {
+                opacity: '0',
+                transform: 'translateY(10px)'
+            },
+            '100%': {
+                opacity: '1',
+                transform: 'translateY(0)'
+            },
+        },
+        'fade-out-up': {
+            'from': {
+                opacity: '1',
+                transform: 'translateY(0px)'
+            },
+            'to': {
+                opacity: '0',
+                transform: 'translateY(10px)'
+            },
+        }
+      },
+      animation: {
+          'fade-in-down': 'fade-in-down 0.5s ease-out',
+          'fade-in-up': 'fade-in-up 0.5s ease-out',
+          'fade-out-up': 'fade-out-up 0.5s ease-out'
+      }
+    },
   },
   plugins: [],
 }
